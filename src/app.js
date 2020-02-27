4f7f390e2a6a238f08e2d10182d64cf30e9b085a
basile <biblex1222@gmail.com>
2020-02-27 19:38:59 +0100

    add messages delete connexion.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+// IMPORTS
+require('dotenv').config()
 const Discord = require('discord.js')
+const messages = require('./utils/messages')
+const bot = new Discord.Client()
+const loginToken = process.env.LOGIN_TOKEN
+
+
+// CONNEXION TO DISCORD SERVER
+bot.login(loginToken)
+  .then(() => {
+  console.log(messages.hello.consoleStart)
+})
+  .catch(() => {
+    console.log(messages.error.startingError)
+  })
+
+//SEND A MESSAGE IF EVERY THINK IS OK
+bot.on('ready', () => {
+  try {
+    bot.channels.get(process.env.TEST_CHANNEL).send(messages.hello.start)
+  } catch (e) {
+    console.log('zut')
+  }
+
+})
---
+++ src/utils/messages.js
@@ -0,0 +1,11 @@
+module.exports = {
+  help: "je ne peux pas vous aider por le moment",
+  hello: {
+    start: 'salut je suis la pour vous aider! Voici comment me demmander de l\'aide:``` $*help ```',
+    consoleStart: 'Axel est super motivé il s\'est connecté au discord'
+  },
+  error: {
+    startingError: 'mince... Axel n\'arrive pas à se reveiller'
+  }
+
+}
